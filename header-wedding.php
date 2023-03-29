eac07b676b048b033eb401d4795611955f345c14
bajra <bajra.idabagus@karmagroup.com>
2023-03-29 17:28:00 +0800

    update header and footer

--- header-wedding.php
+++ header-wedding.php
@@ -15,13 +15,6 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-  <!-- <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php echo get_bloginfo(); ?> | <?php echo get_the_title() ?></title>
-  <link rel="profile" href="https://gmpg.org/xfn/11">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/9.1.1/swiper-bundle.min.css" integrity="sha512-ZOhNKDt0whrHDdUeNKjuq2C8hturzwCSuCfrOIMIfy9QDgpSqDEebdkGQXvXh1xHmZOHSelDBrTLj6XlqbHz6g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/dist/wedding.css';  ?>"> -->
   <?php wp_head(); ?>
 </head>
 
