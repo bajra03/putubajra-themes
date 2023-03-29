a20e8712e5cb8d2da4d894eeec785a487f1d3dfc
bajra <bajra.idabagus@karmagroup.com>
2023-03-29 12:45:00 +0800

    update profile section and add acf field

--- page-wedding.php
+++ page-wedding.php
@@ -5,39 +5,80 @@
  */
 get_header('wedding');
 
-$banners = get_field('banner_details');
-$banner_sliders = get_field('banner_slider');
-$banner_sliders_portrait = get_field('banner_slider_portrait');
+// Get All Fields
+$banners = get_field('banner_section');
+$banner_sliders_desktop = get_field('banner_slider_desktop');
+$banner_sliders_mobile = get_field('banner_slider_mobile');
 $intros = get_field('intros');
+$section_groom = get_field('section_groom');
+$groom_slider_mobile = get_field('groom_slider_mobile');
+$groom_slider_desktop = get_field('groom_slider_desktop');
+$section_bride = get_field('section_bride');
+$bride_slider_mobile = get_field('bride_slider_mobile');
+$bride_slider_desktop = get_field('bride_slider_desktop');
+// EOF Get All Fields
+
+$datesBanner = strtotime($banners['banner_date']);
+$bannerDate = date("d", $datesBanner);
+$bannerMonth = date("m", $datesBanner);
+$bannerYear = date("Y", $datesBanner);
+
+$datesGroom = strtotime($section_groom['dob']);
+$groomDate = date("d", $datesGroom);
+$groomMonth = date("m", $datesGroom);
+$groomYear = date("Y", $datesGroom);
+
+$datesBride = strtotime($section_bride['dob']);
+$brideDate = date("d", $datesBride);
+$brideMonth = date("m", $datesBride);
+$brideYear = date("Y", $datesBride);
+
+// print_r($section_bride['socials']['facebook']);
 ?>
 <style>
-hr {
-  background-image: url("<?php echo get_template_directory_uri() . '/assets/images/divider.svg'; ?>");
-}
+  hr {
+    background-image: url("<?php echo get_template_directory_uri() . '/assets/images/divider.svg'; ?>");
+  }
 
-@media (min-width: 1024px) {
-  <?php foreach ($banner_sliders as $key=> $banner) {
-    ?>body .banner__slider .banner__slider__slide.slide<?php echo $key+1;
+  @media (min-width: 768px) {
 
-    ?> {
-      background-image: url('<?php echo $banner['full_image_url']; ?>') !important;
+    /* Banner Section */
+    <?php foreach ($banner_sliders_desktop as $key => $banner) {
+    ?>body .banner__slider .banner__slider__slide.banner__slider__slide--<?php echo $key + 1;
+
+                                                                          ?> {
+      background-image: url('<?php echo $banner["full_image_url"]; ?>') !important;
     }
 
     <?php
+    }
+    ?>
+    /* EOF Banner Section */
+
+    /* Groom Slider */
+    <?php foreach ($groom_slider_desktop as $key => $slide) { ?>body .profile--groom .profile__slider__slide.profile__slider__slide--<?php echo $key + 1; ?> {
+      background-image: url('<?php echo $slide["full_image_url"]; ?>') !important;
+    }
+
+    <?php } ?>
+    /* EOF Groom Slider */
+
+    /* Bride Slider */
+    <?php foreach ($bride_slider_desktop as $key => $slide) { ?>body .profile--bride .profile__slider__slide.profile__slider__slide--<?php echo $key + 1; ?> {
+      background-image: url('<?php echo $slide["full_image_url"]; ?>') !important;
+    }
+
+    <?php } ?>
+    /* EOF Bride Slider */
   }
-
-  ?>
-}
-
 </style>
 
 <!-- Section Banner -->
 <section class="banner">
   <div class="banner__slider swiper-slider">
     <div class="banner__slider__wrapper swiper-wrapper">
-      <?php foreach ($banner_sliders_portrait as $key => $banner) { ?>
-      <div class="swiper-slide banner__slider__slide slide<?php echo $key + 1; ?>"
-        style="background-image: url('<?php echo $banner['full_image_url']; ?>');"></div>
+      <?php foreach ($banner_sliders_mobile as $key => $banner) { ?>
+        <div class="swiper-slide banner__slider__slide banner__slider__slide--<?php echo $key + 1; ?>" style="background-image: url('<?php echo $banner['full_image_url']; ?>');"></div>
       <?php } ?>
     </div>
   </div>
@@ -50,7 +91,7 @@ hr {
         <span class="banner__content__name__bride"><?php echo $banners['bride_name']; ?></span>
       </h1>
     </div>
-    <div class="banner__content__date">17 . 04 . 2023</div>
+    <div class="banner__content__date"><?php echo $bannerDate; ?> . <?php echo $bannerMonth; ?> . <?php echo $bannerYear; ?></div>
     <div class="banner__content__address">
       <i class="fa-solid fa-location-dot"></i> <?php echo $banners['banner_address']; ?>
     </div>
@@ -61,6 +102,11 @@ hr {
 <!-- Section ucapan pengantar -->
 <section class="intro">
   <div class="intro__container">
+    <?php if ($intros["intro_image"]["url"]) { ?>
+      <figure>
+        <img src="<?php echo $intros["intro_image"]["url"]; ?>" alt="Intro image" aria-hidden="true">
+      </figure>
+    <?php } ?>
     <h2 class="text-header"><?php echo $intros['intro_title']; ?></h2>
     <p><?php echo $intros['intro_text']; ?></p>
   </div>
@@ -72,45 +118,42 @@ hr {
   <!-- Slider Profile Groom -->
   <div class="profile__slider profile__slider--groom swiper-slider">
     <div class="swiper-wrapper profile__slider__wrapper">
-      <div class="swiper-slide profile__slider__slide"
-        style="background-image: url('http://localhost/putubajra/wp-content/uploads/2023/03/banner-2.jpg');">
-      </div>
-      <div class="swiper-slide profile__slider__slide"
-        style="background-image: url('http://localhost/putubajra/wp-content/uploads/2023/03/banner-3.jpg');">
-      </div>
-      <div class="swiper-slide profile__slider__slide"
-        style="background-image: url('http://localhost/putubajra/wp-content/uploads/2023/03/banner-1.jpg');">
-      </div>
+      <?php foreach ($groom_slider_mobile as $key => $slide) { ?>
+        <div class="swiper-slide profile__slider__slide profile__slider__slide--<?php echo $key + 1; ?>" style="background-image: url('<?php echo $slide['full_image_url']; ?>');">
+        </div>
+      <?php } ?>
     </div>
+  </div>
   </div> <!-- EOF Slider Profile Groom -->
-
   <!-- Profile Details Groom -->
   <div class="profile__details">
-
     <figure class="profile__picture swiper-wrapper">
-      <img src="http://localhost/putubajra/wp-content/uploads/2023/03/banner-3-p.jpg" alt="Profile picture groom"
-        aria-hidden="true">
+      <img src="<?php echo $section_groom['profile_picture']['url']; ?>" alt="Profile picture groom" aria-hidden="true">
     </figure>
-
     <div class="profile__info">
-      <div class="profile__info__dob">03 . 03</div>
-      <h2 class="profile__info__name text-header">Ida Bagus Putu Bajra</h2>
+      <div class="profile__info__dob"><?php echo $groomDate; ?> . <?php echo $groomMonth; ?></div>
+      <h2 class="profile__info__name text-header"><?php echo $section_groom['name']; ?></h2>
       <hr>
-      <p class="profile__info__desc">
-        Putra pertama dari pasangan:<br>
-        <strong>Ida Bagus Ketut Arjana</strong>
-        <br>&<br>
-        <strong>Ida Ayu Putu Martini</strong>
-        <br><br>
-        Br. Dinas Kauhan, Desa Kekeran, Busungbiu, Buleleng
-      </p>
+      <div class="profile__info__desc">
+        <?php echo $section_groom['description']; ?>
+      </div>
       <hr>
-      <ul class="profile__info__socials">
-        <li><a href="#"><i class="fa-brands fa-instagram social-ig"></i></a></li>
-        <li><a href="#"><i class="fa-brands fa-facebook social-fb"></i></a></li>
-        <li><a href="#"><i class="fa-brands fa-tiktok social-tiktok"></i></a></li>
-        <li><a href="#"><i class="fa-brands fa-linkedin-in social-linkedin"></i></a></li>
-      </ul>
+      <?php if ($section_groom['socials']) { ?>
+        <ul class="profile__info__socials">
+          <?php if ($section_groom['socials']['instagram']) { ?>
+            <li><a href="<?php $section_groom['socials']['instagram']; ?>"><i class="fa-brands fa-instagram social-ig"></i></a></li>
+          <?php } ?>
+          <?php if ($section_groom['socials']['facebook']) { ?>
+            <li><a href="#"><i class="fa-brands fa-facebook social-fb"></i></a></li>
+          <?php } ?>
+          <?php if ($section_groom['socials']['tiktok']) { ?>
+            <li><a href="#"><i class="fa-brands fa-tiktok social-tiktok"></i></a></li>
+          <?php } ?>
+          <?php if ($section_groom['socials']['linkedin']) { ?>
+            <li><a href="#"><i class="fa-brands fa-linkedin-in social-linkedin"></i></a></li>
+          <?php } ?>
+        </ul>
+      <?php } ?>
       <hr>
     </div>
   </div><!-- EOF Profile Details Groom -->
@@ -122,15 +165,10 @@ hr {
   <!-- Slider Profile Bride -->
   <div class="profile__slider profile__slider--bride swiper-slider">
     <div class="swiper-wrapper profile__slider__wrapper">
-      <div class="swiper-slide profile__slider__slide"
-        style="background-image: url('http://localhost/putubajra/wp-content/uploads/2023/03/banner-1.jpg');">
-      </div>
-      <div class="swiper-slide profile__slider__slide"
-        style="background-image: url('http://localhost/putubajra/wp-content/uploads/2023/03/banner-3.jpg');">
-      </div>
-      <div class="swiper-slide profile__slider__slide"
-        style="background-image: url('http://localhost/putubajra/wp-content/uploads/2023/03/banner-2.jpg');">
-      </div>
+      <?php foreach ($bride_slider_mobile as $key => $slide) { ?>
+        <div class="swiper-slide profile__slider__slide profile__slider__slide--<?php echo $key + 1; ?>" style="background-image: url('<?php echo $slide['full_image_url']; ?>');">
+        </div>
+      <?php } ?>
     </div>
   </div> <!-- EOF Slider Profile Bride -->
 
@@ -138,28 +176,32 @@ hr {
   <div class="profile__details">
 
     <figure class="profile__picture swiper-wrapper">
-      <img src="http://localhost/putubajra/wp-content/uploads/2023/03/banner-2-p.jpg" alt="Profile picture bride"
-        aria-hidden="true">
+      <img src="<?php echo $section_bride['profile_picture']['url']; ?>" alt="Profile picture bride" aria-hidden="true">
     </figure>
 
     <div class="profile__info">
-      <div class="profile__info__dob">27 . 02</div>
-      <h2 class="profile__info__name text-header">Ida Ayu Putri Widiantari</h2>
-      <p class="profile__info__desc">
-        Putri pertama dari pasangan:<br><br>
-        <strong>Ida Bagus Rai Diksa</strong>
-        <br>&<br>
-        <strong>Ni Luh Wiarti</strong>
-        <br><br>
-        Jl. Turi No. 61, Br. Ujung, Kesiman, Denpasar
-      </p>
+      <div class="profile__info__dob"><?php echo $brideDate; ?> . <?php echo $brideMonth; ?></div>
+      <h2 class="profile__info__name text-header"><?php echo $section_bride['name']; ?></h2>
+      <div class="profile__info__desc">
+        <?php echo $section_bride['description']; ?>
+      </div>
       <hr>
-      <ul class="profile__info__socials">
-        <li><a href="#"><i class="fa-brands fa-instagram social-ig"></i></a></li>
-        <li><a href="#"><i class="fa-brands fa-facebook social-fb"></i></a></li>
-        <li><a href="#"><i class="fa-brands fa-tiktok social-tiktok"></i></a></li>
-        <li><a href="#"><i class="fa-brands fa-linkedin-in social-linkedin"></i></a></li>
-      </ul>
+      <?php if ($section_bride['socials']) { ?>
+        <ul class="profile__info__socials">
+          <?php if ($section_bride['socials']['instagram']) { ?>
+            <li><a href="<?php echo $section_bride['socials']['instagram']; ?>"><i class="fa-brands fa-instagram social-ig"></i></a></li>
+          <?php } ?>
+          <?php if ($section_bride['socials']['facebook']) { ?>
+            <li><a href="<?php echo $section_bride['socials']['facebook']; ?>"><i class="fa-brands fa-facebook social-fb"></i></a></li>
+          <?php } ?>
+          <?php if ($section_bride['socials']['tiktok']) { ?>
+            <li><a href="<?php echo $section_bride['socials']['tiktok']; ?>"><i class="fa-brands fa-tiktok social-tiktok"></i></a></li>
+          <?php  } ?>
+          <?php if ($section_bride['socials']['linkedin']) { ?>
+            <li><a href="<?php echo $section_bride['socials']['linkedin']; ?>"><i class="fa-brands fa-linkedin-in social-linkedin"></i></a></li>
+          <?php } ?>
+        </ul>
+      <?php } ?>
       <hr>
     </div>
   </div><!-- EOF Profile Details Bride -->
