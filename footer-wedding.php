21eb1bfe47fc57da200ce4b1de124fd59b40d8a7
bajra03 <putu.bajra@gmail.com>
2023-04-04 22:30:02 +0800

    update footer

--- footer-wedding.php
+++ footer-wedding.php
@@ -35,17 +35,7 @@
 
 <footer id="colophon" class="site-footer">
   <div class="site-info">
-    <a href="<?php echo esc_url(__('https://wordpress.org/', 'putubajra')); ?>">
-      <?php
-            /* translators: %s: CMS name, i.e. WordPress. */
-            printf(esc_html__('Proudly powered by %s', 'putubajra'), 'WordPress');
-            ?>
-    </a>
-    <span class="sep"> | </span>
-    <?php
-        /* translators: 1: Theme name, 2: Theme author. */
-        printf(esc_html__('Theme: %1$s by %2$s.', 'putubajra'), 'putubajra', '<a href="http://underscores.me/">Underscores.me</a>');
-        ?>
+    Develop by <a href="<?php echo get_home_url(); ?>"> Bajra</a>, with full of <i class="fa-solid fa-heart"></i>
   </div><!-- .site-info -->
 </footer><!-- #colophon -->
 
