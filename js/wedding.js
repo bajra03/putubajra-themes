ba17641918abeab47ea06a962dfe5babde4c0334
bajra <bajra.idabagus@karmagroup.com>
2023-04-05 10:35:19 +0800

    update fixbody

--- js/wedding.js
+++ js/wedding.js
@@ -1,149 +1,150 @@
-document.body.classList.add('fixbody');
+document.body.classList.add("fixbody");
+window.addEventListener(
+  "load",
+  function () {
+    console.log("content loaded");
+    AOS.init({
+      once: true,
+    });
 
-AOS.init({
-    once: true
-});
+    const generalSliderOpt = {
+      effect: "fade",
+      loop: true,
+      autoplay: {
+        delay: 5000,
+      },
+    };
 
-const generalSliderOpt = {
-  effect: 'fade',
-  loop: true,
-  autoplay: {
-    delay: 5000,
-  },
-};
+    const generalSlider = (elm) => {
+      return new Swiper(elm, generalSliderOpt);
+    };
 
-const generalSlider = (elm) => {
-  return new Swiper(elm, generalSliderOpt);
-};
-
-const sliderWithThumb = (elm, elmThumb) => {
-  return new Swiper(elm, {
-    effect: 'fade',
-    loop: true,
-    autoplay: true,
-    thumbs: {
-      swiper: new Swiper (elmThumb, {
-        spaceBetween: 5,
-        slidesPerView: 3,
-        watchSlidesProgress: true,
-        watchSlidesVisibility: true,
+    const sliderWithThumb = (elm, elmThumb) => {
+      return new Swiper(elm, {
+        effect: "fade",
         loop: true,
         autoplay: true,
-      }),
-    },
-  });
-};
+        thumbs: {
+          swiper: new Swiper(elmThumb, {
+            spaceBetween: 5,
+            slidesPerView: 3,
+            watchSlidesProgress: true,
+            watchSlidesVisibility: true,
+            loop: true,
+            autoplay: true,
+          }),
+        },
+      });
+    };
 
-generalSlider('.banner__slider');
-generalSlider('.profile__slider--groom');
-generalSlider('.profile__slider--bride');
+    generalSlider(".banner__slider");
+    generalSlider(".profile__slider--groom");
+    generalSlider(".profile__slider--bride");
 
-// Countdown date
-const countdownDate = new Date("Apr 17, 2023 17:00:00").getTime();
+    // Countdown date
+    const countdownDate = new Date("Apr 17, 2023 17:00:00").getTime();
 
-const countDate = setInterval(function ()
-{
-  const now = new Date().getTime();
-  const distance = countdownDate - now;
+    const countDate = setInterval(function () {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      // Time calculations for days, hours, minutes and seconds
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+      document.querySelector(".countdown__timer__day").innerHTML = days;
+      document.querySelector(".countdown__timer__hour").innerHTML = hours;
+      document.querySelector(".countdown__timer__minute").innerHTML = minutes;
+      document.querySelector(".countdown__timer__second").innerHTML = seconds;
 
-  document.querySelector(".countdown__timer__day").innerHTML = days;
-  document.querySelector(".countdown__timer__hour").innerHTML = hours;
-  document.querySelector(".countdown__timer__minute").innerHTML = minutes;
-  document.querySelector(".countdown__timer__second").innerHTML = seconds;
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(countDate);
+        document.querySelector(".countdown__timer__day").innerHTML = "0";
+        document.querySelector(".countdown__timer__hour").innerHTML = "0";
+        document.querySelector(".countdown__timer__minute").innerHTML = "0";
+        document.querySelector(".countdown__timer__second").innerHTML = "0";
+      }
+    }, 1000);
+    // EOF Countdown date
 
-  // If the count down is over, write some text
-  if (distance < 0) {
-    clearInterval(countDate);
-    document.querySelector(".countdown__timer__day").innerHTML = '0';
-    document.querySelector(".countdown__timer__hour").innerHTML = '0'
-    document.querySelector(".countdown__timer__minute").innerHTML = '0';
-    document.querySelector(".countdown__timer__second").innerHTML = '0';
-  }
-}, 1000);
-// EOF Countdown date
+    // Galleries
+    const grid = document.getElementById("galleries");
 
-// Galleries
-const grid = document.getElementById('galleries');
+    const iso = new Isotope(grid, {
+      itemSelector: ".grid-item",
+      percentPosition: true,
+      masonry: {
+        columnWidth: ".grid-sizer",
+        // gutter: 10
+      },
+    });
+    // EOF Galleries
 
-const iso = new Isotope(grid, {
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  masonry: {
-    columnWidth: '.grid-sizer',
-    // gutter: 10
-  }
-});
-// EOF Galleries
+    // Scroll top
+    const btnToTop = document.getElementById("btn-to-top");
+    const btnToggleAudio = document.getElementById("btn-toggle-audio");
 
-// Scroll top
-const btnToTop = document.getElementById('btn-to-top');
-const btnToggleAudio = document.getElementById('btn-toggle-audio');
+    btnToTop.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    });
 
-btnToTop.addEventListener('click', function (e)
-{
-  e.preventDefault();
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
+    const toTopBtnDisplay = function () {
+      window.scrollY > 200 ? btnToTop.parentElement.classList.add("show") : btnToTop.parentElement.classList.remove("show");
+      window.scrollY > 200 ? btnToggleAudio.parentElement.classList.add("show") : btnToggleAudio.parentElement.classList.remove("show");
+    };
 
-const toTopBtnDisplay = function () {
-    window.scrollY > 200 ? btnToTop.parentElement.classList.add("show") : btnToTop.parentElement.classList.remove("show");
-    window.scrollY > 200 ? btnToggleAudio.parentElement.classList.add("show") : btnToggleAudio.parentElement.classList.remove("show");
-};
+    window.addEventListener("scroll", toTopBtnDisplay);
+    // EOF Scroll top
 
-window.addEventListener("scroll", toTopBtnDisplay);
-// EOF Scroll top
+    // Toggle Audio Play & Stop
+    const myAudio = document.getElementById("myAudio");
+    const btnAudioToggle = document.getElementById("btn-toggle-audio");
+    let isPlaying = true;
 
-// Toggle Audio Play & Stop
-const myAudio = document.getElementById('myAudio');
-const btnAudioToggle = document.getElementById('btn-toggle-audio');
-let isPlaying = true;
+    btnAudioToggle.addEventListener("click", function (e) {
+      e.preventDefault();
+      toggleAudio();
+    });
 
-btnAudioToggle.addEventListener('click', function (e) {
-    e.preventDefault();
-    toggleAudio();
-});
+    function toggleAudio() {
+      isPlaying ? playAudio() : pauseAudio();
+    }
 
-function toggleAudio() {
-    isPlaying ? playAudio() : pauseAudio();
-}
+    function playAudio() {
+      myAudio.play();
+      isPlaying = !isPlaying;
+      btnAudioToggle.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    }
 
-function playAudio() {
-    myAudio.play();
-    isPlaying = !isPlaying;
-    btnAudioToggle.innerHTML = '<i class="fa-solid fa-pause"></i>';
-}
+    function pauseAudio() {
+      myAudio.pause();
+      isPlaying = !isPlaying;
+      btnAudioToggle.innerHTML = '<i class="fa-solid fa-play"></i>';
+    }
+    // EOF Toggle Audio Play & Stop
 
-function pauseAudio() {
-    myAudio.pause();
-    isPlaying = !isPlaying;
-    btnAudioToggle.innerHTML = '<i class="fa-solid fa-play"></i>';
-}
-// EOF Toggle Audio Play & Stop
+    // Get invited name
+    const url = window.location.search;
+    const urlParams = new URLSearchParams(url);
+    const btnOpenInvitation = document.getElementById("btn-open-invitation");
+    const popupInvitationEl = document.getElementById("popup-invitation");
 
-// Get invited name
-const url = window.location.search;
-const urlParams = new URLSearchParams(url);
-const btnOpenInvitation = document.getElementById('btn-open-invitation');
-const popupInvitationEl = document.getElementById('popup-invitation');
+    if (urlParams.get("to")) {
+      document.querySelector(".popup-invitation__details__name").innerHTML = urlParams.get("to");
+    }
 
-if (urlParams.get('to'))
-{
-  document.querySelector('.popup-invitation__details__name').innerHTML = urlParams.get('to');
-}
-btnOpenInvitation.addEventListener('click', function (e)
-{
-  e.preventDefault();
-  popupInvitationEl.classList.add('hide');
-  btnAudioToggle.click();
-  document.body.classList.remove('fixbody');
-});
-// EOF Get invited name
-
+    btnOpenInvitation.addEventListener("click", function (e) {
+      e.preventDefault();
+      popupInvitationEl.classList.add("hide");
+      btnAudioToggle.click();
+      document.body.classList.remove("fixbody");
+    });
+    // EOF Get invited name
+  },
+  false
+); // EOF Document on load
